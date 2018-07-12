489092d2d71280d37c85842dde89789eb8a3d432
leondo <leondo123@gmail.com>
2018-07-11 23:16:14 -0600

    MVP

--- index.js
+++ index.js
@@ -3,28 +3,57 @@ const { dMatrix } = require('./dMatrix')
 const { shortestPath } = require('./shortestPath')
 const { requestData } = require('./requestData')
 
-const vertices = 4
-
-// let distMatrix be a array of minimum distances initialized to infinity
-const distMatrix = dMatrix(vertices)
-
-// let nextMatrix be an array of vertex indices initialized to null
-const nextMatrix = nMatrix(vertices)
-
 // request data
 const requests = requestData()
 
-// map source_name to an index
+// map source_name and destination_name to an index for machine readability
+// map index to source_name and destination_name for human readability
+const nameToIndex = {}
+const indexToName = {}
+let index = 0
+for (let i in requests) {
+    sourceName = requests[i].source_name
+    destinationName = requests[i].destination_name
+    // only map unique names
+    if (nameToIndex[sourceName] === undefined) {
+        nameToIndex[sourceName] = index
+        indexToName[index] = sourceName
+        index++
+    }
+    // only map unique names
+    if (nameToIndex[destinationName] === undefined) {
+        nameToIndex[destinationName] = index
+        indexToName[index] = sourceName
+        index++
+    }
+}
+
+nameToIndex.one = 0
+nameToIndex.two = 1
+nameToIndex.three = 2
+nameToIndex.four = 3
+indexToName[0] = 'one'
+indexToName[1] = 'two'
+indexToName[2] = 'three'
+indexToName[3] = 'four'
 
 // loop through to construct all the paths
 const paths = []
 for (let i in requests) {
+    const sourceName = requests[i].source_name
+    const destinationName = requests[i].destination_name
     paths.push({
-        source: requests[i].source_name,
-        destination: requests[i].destination_name,
+        source: nameToIndex[sourceName],
+        destination: nameToIndex[destinationName],
         value: requests[i].source_value / requests[i].destination_value
     })
 }
+
+// let nextMatrix be an array of vertex indices initialized to null
+const nextMatrix = nMatrix(index) // index is also total number of verticies 🤯
+
+// let distMatrix be a array of minimum distances initialized to infinity
+const distMatrix = dMatrix(index)
 
 // loop through paths to update matrix
 for (i in paths) {
@@ -48,9 +77,23 @@ for (let k = 0; k < distMatrix.length; k++) {
         }
     }
 }
+
+console.log('\n distMatrix', distMatrix)
+console.log('\n nextMatrix', nextMatrix)
+
 const expecrtedDistMatrix = '[[0,-1,-2,0],[4,0,2,4],[5,1,0,2],[3,-1,1,0]]'
 const expectedNextMatric = '[[null,2,2,2],[0,null,0,0],[3,3,null,3],[1,1,1,null]]'
 console.log(JSON.stringify(distMatrix) === expecrtedDistMatrix)
 console.log(JSON.stringify(nextMatrix) === expectedNextMatric)
 
-const path = shortestPath(1, 0, nextMatrix)
+// print the paths
+for (let i = 0; i < index; i++) {
+    for (let j = 0; j < index; j++) {
+        const sourceName = indexToName[i]
+        const destinationName = indexToName[j]
+        const path = shortestPath(i, j, nextMatrix).map(val => indexToName[val])
+        const distance = distMatrix[i][j]
+        // prettier-ignore
+        console.log(`the best path from ${sourceName} to ${destinationName} is ${JSON.stringify(path)} at a distance of ${distance}`)
+    }
+}
--- requestData.js
+++ requestData.js
@@ -1,37 +1,40 @@
-// fetches data and normalizes it
+/*
+Requests will be:
+<source_name> <source_value> <destination_name> <destination_value>
+*/
 function requestData() {
     const request0 = {
-        source_name: 0,
+        source_name: 'one',
         source_value: -2,
-        destination_name: 2,
+        destination_name: 'three',
         destination_value: 1
     }
 
     const request1 = {
-        source_name: 2,
+        source_name: 'three',
         source_value: 2,
-        destination_name: 3,
+        destination_name: 'four',
         destination_value: 1
     }
 
     const request2 = {
-        source_name: 3,
+        source_name: 'four',
         source_value: -1,
-        destination_name: 1,
+        destination_name: 'two',
         destination_value: 1
     }
 
     const request3 = {
-        source_name: 1,
+        source_name: 'two',
         source_value: 4,
-        destination_name: 0,
+        destination_name: 'one',
         destination_value: 1
     }
 
     const request4 = {
-        source_name: 1,
+        source_name: 'two',
         source_value: 3,
-        destination_name: 2,
+        destination_name: 'three',
         destination_value: 1
     }
 
